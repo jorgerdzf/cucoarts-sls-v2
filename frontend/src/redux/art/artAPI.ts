4849f3b36321c4a3de93cebfff6700ca113f3006
Jorge Rdz <jorgerdzf@gmail.com>
2023-04-18 17:32:03 -0600

    solve cors issues with frontend

--- frontend/src/redux/art/artAPI.ts
+++ frontend/src/redux/art/artAPI.ts
@@ -1,12 +1,8 @@
 import axios from 'axios';
 import { apiEndpoint } from '../../constants/settings';
-import { headers } from '../../constants/headers';
 
 export const getRandomArt = async () => {
     const url = `${apiEndpoint}art` || '';
-    const options = {
-        headers: headers,
-      };
     try {
         const response = await axios.get(url);
         return response;
